3f58fe8a064c7ad418ec24540b2efa098a8b1f19
NooranTarek <noort.mohamed@gmail.com>
2024-04-22 15:11:33 +0200

    adding post folder in views with index file and adding create for one  post in post controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,6 @@
 class PostController < ApplicationController
     def index
+        post=Post.create({title:"last news",content:"the installation of rails is done successfullyyyyy"})
+        puts post.inspect
     end
 end
