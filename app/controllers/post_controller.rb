75f996f79d2a7c5b68b459591db07e3f64da32fd
NooranTarek <noort.mohamed@gmail.com>
2024-04-27 02:12:18 +0300

    adding the crud operations of post and the validation of auther

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,42 @@
 class PostController < ApplicationController
+    #__________________________________________________________________________
     def index
-        post=Post.create({title:"last news",content:"the installation of rails is done successfullyyyyy"})
-        puts post.inspect
+     	@posts=Post.all
+    end
+    #__________________________________________________________________________
+    def new 
+      	@post=Post.new
+    end
+    #__________________________________________________________________________ 
+    def create
+        @post = Post.new(title: params[:title], content: params[:content])
+        if @post.save 
+        redirect_to @post
+        else
+        render :new 
+        end
+    end
+    #__________________________________________________________________________  
+    def show
+      	@post=Post.find(params[:id])
+    end
+    #__________________________________________________________________________  
+    def edit
+      	@post = Post.find(params[:id])
+    end
+    #__________________________________________________________________________  
+    def update
+          @post = Post.find(params[:id])
+          if @post.update(post_params)
+              redirect_to @post
+          else
+              render :edit
+          end
+      end
+    #__________________________________________________________________________  
+    def destroy
+        @post = post.find(params[:id]) 
+        @post.destroy
+        redirect_to @post
     end
 end
