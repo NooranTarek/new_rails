f1ca1aa9237408adba53295e8bfae9b7f89e7910
NooranTarek <noort.mohamed@gmail.com>
2024-05-03 01:04:32 +0300

    agging creator to posts table

--- db/migrate/20240502210734_create_posts.rb
+++ db/migrate/20240502210734_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
       t.string :title
       t.string :content
       t.references :author, null: false, foreign_key: true
+      t.references :creator, null: false, foreign_key: { to_table: :authors }
 
       t.timestamps
     end
