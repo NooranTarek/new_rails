95f1646bc53d5d8656be5a62c176d17a7ddc71f3
NooranTarek <noort.mohamed@gmail.com>
2024-05-03 01:07:35 +0300

    migrate emoji

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_210734) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_02_220629) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.date "dob"
     t.string "email"
     t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "emojis", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
